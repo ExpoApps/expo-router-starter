0a4b04e0e93f4e4fe7bb6a7c77e85996da6dec56
Johan Melin <soldierheart@hotmail.com>
2024-05-08 14:20:38 +0200

    themed card

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,25 +2,14 @@ import { Image, StyleSheet } from 'react-native'
 import { View, Text } from '@/components/Themed';
 import React from 'react'
 import Button from './Button'
-
-const palette = {
-    primary95: 'rgb(246, 237, 255)',
-    primary90: 'rgb(234, 221, 255)',
-    primary80: 'rgb(208, 188, 255)',
-    primary70: 'rgb(182, 157, 248)',
-    primary60: 'rgb(154, 130, 219)',
-    primary50: 'rgb(127, 103, 190)',
-    primary40: 'rgb(103, 80, 164)',
-    primary30: 'rgb(79, 55, 139)',
-    primary20: 'rgb(56, 30, 114)',
-    primary10: 'rgb(33, 0, 93)',
-}
+import { useColors } from '@/constants/Colors';
 
 const Card = () => {
+    const colors = useColors();
   return (
     <View style={styles.container}>
         <Image source={require('../assets/images/photos/ocean.jpg')} style={styles.image} />
-        <View style={styles.bottom}>
+        <View style={[{...styles.bottom, backgroundColor: colors.backgroundColor }]}>
             <Text>Fish</Text>
             <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in pretium ante. Maecenas finibus, sem in convallis tincidunt, elit enim mollis massa. </Text>
             <View style={{ ...styles.bottom, flexDirection: 'row', justifyContent: 'flex-end' }}>
@@ -45,7 +34,6 @@ const styles = StyleSheet.create({
         width: '100%',
     },
     bottom: {
-        backgroundColor: palette.primary10,
         padding: 5,
     },
 });
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,7 +1,35 @@
+import { useTheme } from "@/context/ThemeContext";
+
 const white = '#fff';
 const black = '#000';
 const grey = '#8E8E8F';
 const blue = '#2f95dc';
+
+const palette = {
+  primary95: 'rgb(246, 237, 255)',
+  primary90: 'rgb(234, 221, 255)',
+  primary80: 'rgb(208, 188, 255)',
+  primary70: 'rgb(182, 157, 248)',
+  primary60: 'rgb(154, 130, 219)',
+  primary50: 'rgb(127, 103, 190)',
+  primary40: 'rgb(103, 80, 164)',
+  primary30: 'rgb(79, 55, 139)',
+  primary20: 'rgb(56, 30, 114)',
+  primary10: 'rgb(33, 0, 93)',
+}
+
+const darkColors = {
+  backgroundColor: palette.primary10
+}
+
+const lightColors = {
+  backgroundColor: palette.primary90
+}
+
+export function useColors() {
+  const { theme } = useTheme();
+  return theme === 'dark' ? darkColors : lightColors
+}
 
 export default {
   light: {
